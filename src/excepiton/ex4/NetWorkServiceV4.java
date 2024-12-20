80d353dac8c13e0189b42015b2abe3cf2b6ca709
zncho <4pril.2nd@gmail.com>
2024-12-20 17:16:23 +0900

    예외 처리 2 (실습) - 실무 예외 처리 방안

---
+++ src/excepiton/ex4/NetWorkServiceV4.java
@@ -0,0 +1,17 @@
+package excepiton.ex4;
+
+public class NetWorkServiceV4 {
+
+	public void sendMessage(String data) {
+		String address = "http://example.com";
+		NetworkClientV4 client = new NetworkClientV4(address);
+		client.initError(data); //추가
+
+		try {
+			client.connect();
+			client.send(data);
+		} finally {
+			client.disconnect();
+		}
+	}
+}
